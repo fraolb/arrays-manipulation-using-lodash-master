7a373f1156eb3b0e7cea32dd8e77c4143cdf2784
fraolb <Fraolbereket@gmail.com>
2023-05-11 19:20:16 +0300

    lodash

--- src/LodashExample.js
+++ src/LodashExample.js
@@ -1,17 +1,23 @@
 
 //import the lodash module
-
+const lodash = require('lodash')
 
 //Create a function to find a maximum value from number array.
-
+function findMaxValue(arr){
+  return lodash.max(arr)
+}
 
 
 //Create a function to return all values from numbers array 
 //which are greater than the second parameter.​
-
+function filterValues(arr, num){
+  return lodash.filter(arr, (i)=>i > num)
+}
 
 //Create a function to return all values of employeeName array in capital letters.
-
+function nameInCapital(arr){
+  return arr.map(i => lodash.capitalize(i))
+}
 
 
 
@@ -21,3 +27,10 @@ module.exports = {
   nameInCapital,
   
 }
+
+const a =[2,4,6,245,75,13]
+const b =["abebe","kebede","mule"]
+
+console.log(findMaxValue(a))
+console.log(filterValues(a,13))
+console.log(nameInCapital(b))
